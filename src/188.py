243f43e83e27874313f919bb74156400816a362c
Nathalia Lorena <nathalialorenacardoso@gmail.com>
2022-04-18 01:38:02 -0300

    Adicionando solução do problema 188 do Leetcode
    Co-Authored-By: Francisco Heronildo <heronildo.2010@hotmail.com>

---
+++ src/188.py
@@ -0,0 +1,28 @@
+import math
+
+def maxProfit(k, prices):
+    if k >= len(prices) // 2:
+      sell = 0
+      hold = -math.inf
+
+      for price in prices:
+        sell = max(sell, hold + price)
+        hold = max(hold, sell - price)
+
+      return sell
+
+    sell = [0] * (k + 1)
+    hold = [-math.inf] * (k + 1)
+
+    for price in prices:
+      for i in range(k, 0, -1):
+        sell[i] = max(sell[i], hold[i] + price)
+        hold[i] = max(hold[i], sell[i - 1] - price)
+
+    return sell[k]
+
+
+k = 2
+prices = [3,2,6,5,0,3]
+
+print(maxProfit(k, prices))
