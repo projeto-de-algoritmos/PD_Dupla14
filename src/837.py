727bc58034dd5987b2c039edadcf2e07367a9673
FranciscoHeronildo <heronildo.2010@hotmail.com>
2022-04-18 19:44:54 -0300

    Adicionando solucao do problema 837 do leetcode
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>

---
+++ src/837.py
@@ -0,0 +1,27 @@
+
+def new21Game(n, k, maxPts):
+    
+    if k == 0 or n >= k - 1 + maxPts:
+      return 1.0
+
+    ans = 0.0
+    dp = [1.0] + [0] * n  
+    windowSum = dp[0]  
+
+    for i in range(1, n + 1):
+
+      dp[i] = windowSum / maxPts
+      if i < k:
+        windowSum += dp[i]
+      else:  
+        ans += dp[i]
+      if i - maxPts >= 0:
+        windowSum -= dp[i - maxPts]
+
+    return ans
+
+n = 6
+k = 1
+maxPts = 10
+
+print(new21Game(n, k, maxPts))
