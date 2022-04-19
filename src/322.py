f8514ba7344d8331db1f689a25fb29fdf67c0f4f
FranciscoHeronildo <heronildo.2010@hotmail.com>
2022-04-18 21:07:37 -0300

    Adicionando comentarios

--- src/322.py
+++ src/322.py
@@ -2,13 +2,15 @@ def coinChange(coins, amount):
     
     dp = [0] + [amount + 1] * amount
 
+    # verifica todas as moeda em moedas
     for coin in coins:
       for i in range(coin, amount + 1):
+        # dp[i] = menor * de moedas para fazer i
         dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return -1 if dp[amount] == amount + 1 else dp[amount]
 
-coins = [1]
-amount = 0
+coins = [1,2,5]
+amount = 11
 
 print(coinChange(coins, amount))
--- src/837.py
+++ src/837.py
@@ -1,12 +1,16 @@
 
 def new21Game(n, k, maxPts):
-    
+  # quando o jogo termina, o ponto está em [k..k - 1 + maxPts]
+    # P = 1, se n >= k - 1 + maxPts
+    # P = 0, se n < k (observe as restrições já possuem k <= n)
     if k == 0 or n >= k - 1 + maxPts:
       return 1.0
 
     ans = 0.0
-    dp = [1.0] + [0] * n  
-    windowSum = dp[0]  
+    # dp[i] := provar ter em pontos
+    dp = [1.0] + [0] * n
+    # P(i - 1) + P(i - 2) + ... + P(i - maxPts)
+    windowSum = dp[0]
 
     for i in range(1, n + 1):
 
@@ -20,7 +24,7 @@ def new21Game(n, k, maxPts):
 
     return ans
 
-n = 6
+n = 10
 k = 1
 maxPts = 10
 
